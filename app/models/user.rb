3e45b04131289fdd6e7b75086d1ddf23f06ada68
wawer77 <tomek.06@wp.pl>
2018-09-16 11:34:15 +0200

    Remove confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable
 
   has_many :ownerships
   has_many :owned_groups, through: :ownerships, source: :group
